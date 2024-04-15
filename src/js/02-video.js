fb70d405a75e4a5945303523b8da6f733dc38eef
Joanna <joanna_pinczuk@hotmail.com>
2024-04-15 12:43:36 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,20 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const saveCurrentTime = player.getCurrentTime();
 localStorage.setItem('videoplayer-current-time', saveCurrentTime);
 
-const currentTimeFromStorage = () => {
-  const currentTime = localStorage.getItem('videoplayer-current-time');
-  if (currentTime) {
-    player.saveCurrentTime(parseFloat(currentTime));
-  }
-};
+const currentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+if (currentTime) {
+  player.setCurrentTime(parseFloat(currentTime));
+}
 
-player.on('timeupdate', throttle(saveCurrentTime, 1000));
-window.addEventListener('DOMContentLoaded', currentTimeFromStorage);
+player.on('timeupdate', event => {
+  currentTime(event);
+});
+window.addEventListener('DOMContentLoaded', currentTime);
